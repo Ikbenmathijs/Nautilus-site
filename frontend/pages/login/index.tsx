fe85f83876877e4a9e3c1c224b5f5ee422283449
hugo <hugo>
2022-12-06 13:28:25 +0000

    login pagina css

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -5,7 +5,19 @@ import LoginButton from "../../components/loginButton";
 export default function Home() {
 
 
-    return (<div>
-        <LoginButton />
-    </div>)
+    return (<>
+    <div className="">
+        <div className="bg-fixed bg-[url('https://cdn.mos.cms.futurecdn.net/v6XoEzDajGRMWNeLY5NMSb-1920-80.jpg.webp')]">
+            <div className='grid static place-content-center h-screen w-screen backdrop-blur-md'>
+                <div className='static bg-white border rounded-lg p-5'>
+                    <div className="mb-5">
+                        <div className='text-2xl text-slate-700 text-center'>Login</div>
+                        <div className='text-lg text-slate-500'>login met google om de Server te joinen</div>
+                    </div>
+                    <div className="grid static place-content-center"><LoginButton /></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>)
 }
