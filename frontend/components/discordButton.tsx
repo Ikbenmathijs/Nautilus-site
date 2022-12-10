a924bad6b10578cd13af6c2e1e5a6f5bcf47c0fb
Mathijs <Mathijs>
2022-12-10 14:27:44 +0100

    fix text on dark theme

--- frontend/components/discordButton.tsx
+++ frontend/components/discordButton.tsx
@@ -1,5 +1,5 @@
 export default function DiscordButton() {
-    return (<a href="https://discord.com/api/oauth2/authorize?client_id=1049305516385501235&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord&response_type=code&scope=identify%20guilds.join">
+    return (<a className="dark:text-white" href="https://discord.com/api/oauth2/authorize?client_id=1049305516385501235&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord&response_type=code&scope=identify%20guilds.join">
         Join de discord server!
     </a>)
 }
