e534ebded1b0c88f419edff06e488e2dee3c0e19
Mathijs <Mathijs>
2022-12-17 15:33:27 +0100

    fix discord oauth url

--- frontend/components/discordButton.tsx
+++ frontend/components/discordButton.tsx
@@ -1,6 +1,6 @@
 export default function DiscordButton() {
     return (<>
-        <a href="https://discord.com/api/oauth2/authorize?client_id=1049305516385501235&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord&response_type=code&scope=identify%20guilds.join">
+        <a href={process.env.NEXT_PUBLIC_DISCORD_OAUTH_URL}>
             <div className="float-left p-3 border rounded-lg bg-indigo-400 hover:bg-indigo-500 hover:shadow-inner text-white font-medium text-md">Discord</div>
         </a>
     </>)
